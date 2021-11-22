34b43ad5af4bd94357b975bd5e024e58efd7233d
math-yew <math.u@hotmail.com>
2021-11-22 15:31:45 -0700

    correct size

--- script.js
+++ script.js
@@ -13,11 +13,65 @@ ctx.moveTo(95,65);
 ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
 ctx.stroke();
 
-document.querySelector("button").addEventListener("click", function() {
+var fonts = ["none", "raleway"];
+
+var dropdown = document.getElementById("fonts");
+for(var i = 0; i < fonts.length; i++) {
+    var opt = fonts[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    dropdown.appendChild(el);
+}
+
+function updatePosition() {
+  var position = document.getElementById("position").value + "%";
+  console.log("position: " + position);
+  document.getElementById("slide").style.top = position;
+}
+
+function updateSize() {
+  var size = document.getElementById("fontSize").value;
+  var largerSize = size*1 + 5;
+  size = size + "px";
+  largerSize = largerSize + "px";
+  console.log("size: " + size);
+  document.getElementById("name").style.fontSize = size;
+  document.getElementById("name").style.lineHeight = largerSize;
+  document.getElementById("name").style.height = largerSize;
+}
+
+function changeFont(){
+  console.log("changeFont");
+  removeFonts();
+  let newFont = document.getElementById('fonts').value;
+  console.log("newFont: " + newFont);
+  document.getElementById("name").classList.add(newFont);
+}
+
+function changePosition() {
+  document.getElementById("name").style.color = "blue";
+}
+
+function  removeFonts(){
+  for(var i = 0; i < fonts.length; i++) {
+    var element = document.getElementById("name");
+    element.classList.remove(fonts[i]);
+  }
+}
+
+document.querySelector("#generate").addEventListener("click", function(){
+  var name = document.getElementById("nameInput").value.toUpperCase();
+  console.log("name: " + name);
+document.getElementById("name").innerHTML = name;
+document.getElementById("letter").innerHTML = name[0];
+})
+
+document.querySelector("#create").addEventListener("click", function() {
     html2canvas(document.querySelector("#content"), {
       canvas: canvas,
       useCORS: true,
-      scale: 4,
+      scale: 7,
       allowTaint: true,
       letterRendering: true
     }).then(function(canvas) {
